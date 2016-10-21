1db03e1b9c9f524fcff5fe63a3a39ba1fa0b55ad
Chris Richey <chris.j.richey@gmail.com>
2016-10-20 20:55:41 -0700

    Intial Project Upload

---
+++ Assets/Scripts/TrainMover.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrainMover : MonoBehaviour {
+
+	public float power = 1f;
+	public float control;
+	public Rigidbody rb;
+
+	public movement move = new forward();
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate() 
+	{
+		move.move (ref control);
+		rb.AddForce(transform.forward * control * power);
+	}
+}
+
+public abstract class movement
+{
+	public abstract void move(ref float x);
+}
+
+public class backward : movement
+{
+	public override void move(ref float x)
+	{
+		x = -1f;
+	}
+}
+
+public class forward : movement 
+{
+	public override void move(ref float x)
+	{
+		x = 1f;
+	}
+}
+
+public class stop : movement
+{
+	public override void move(ref float x)
+	{
+		x = 0f;
+	}
+}
---
+++ Assets/Scripts/trainstate.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class state
+{
+	public abstract void changeToMyState(TrainMover t);
+}
+
+public class backwardstate : state
+{
+	public override void changeToMyState(TrainMover t)
+	{
+		t.move = new backward ();
+	}
+}
+
+public class forwardstate : state
+{
+	public override void changeToMyState(TrainMover t)
+	{
+		t.move = new forward ();
+	}
+}
+
+public class stopstate : state
+{
+	public override void changeToMyState(TrainMover t)
+	{
+		t.move = new stop ();
+	}
+}
+
+
+public class trainstate : MonoBehaviour {
+
+	state mystate;
+
+	state holderforward = new forwardstate();
+	state holderbackward = new backwardstate();
+	state holderstop = new stopstate();
+
+	public TrainMover t;
+	public int which = 0;
+
+	void Update () {
+		switch (which){
+		case 0:
+			mystate = holderforward;
+			mystate.changeToMyState(t);
+			break;
+		case 1:
+			mystate = holderbackward;
+			mystate.changeToMyState(t);
+			break;
+		case 2:
+			mystate = holderstop;
+			mystate.changeToMyState(t);
+			break;
+		}
+	}
+}
